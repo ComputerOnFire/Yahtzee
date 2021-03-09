f6803148d970d376d24879cc3d9ce92a5b7916ad
John <computeronfire@protonmail.com>
2021-03-09 11:28:21 -0600

    relative path

--- src/main/java/com/github/computeronfire/Yahtzee/Main.java
+++ src/main/java/com/github/computeronfire/Yahtzee/Main.java
@@ -6,13 +6,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.Objects;
-
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("yahtzee.fxml")));
+        Parent root = FXMLLoader.load(getClass().getResource("/yahtzee.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
