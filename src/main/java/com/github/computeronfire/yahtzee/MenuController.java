f99cb2af3bf17a1e398bdf05412b4df5ec5cc882
John <computeronfire@protonmail.com>
2021-03-11 22:28:53 -0600

    add main menu to UI

---
+++ src/main/java/com/github/computeronfire/yahtzee/MenuController.java
@@ -0,0 +1,24 @@
+package com.github.computeronfire.yahtzee;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MenuController {
+
+    public void startGame(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/yahtzeeGame.fxml"));
+
+        Parent parent  = fxmlLoader.load();
+        GameController controller = fxmlLoader.getController();
+        Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(parent, 300, 275);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
