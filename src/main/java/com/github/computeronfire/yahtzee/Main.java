aac30f5171bc6b6498c7986e680e40dd668577ec
John <computeronfire@protonmail.com>
2021-03-22 02:57:07 -0500

    change title

--- src/main/java/com/github/computeronfire/yahtzee/Main.java
+++ src/main/java/com/github/computeronfire/yahtzee/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/yahtzeeMenu.fxml"));
-        primaryStage.setTitle("Yahtzee");
+        primaryStage.setTitle("Let's Play Yahtzee!");
         primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
     }
