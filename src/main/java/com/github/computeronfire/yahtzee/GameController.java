b193b08a1f6aab24198861b2acb0757fbfdfa1cd
John <computeronfire@protonmail.com>
2021-03-22 02:56:51 -0500

    dice button functions, test to check score text

--- src/main/java/com/github/computeronfire/yahtzee/GameController.java
+++ src/main/java/com/github/computeronfire/yahtzee/GameController.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 
 public class GameController {
     public Label helloWorld;
+    public Label p1name;
     public Label p1t1;
     public Label p1t2;
     public Label p1t3;
@@ -20,16 +21,72 @@ public class GameController {
     public Label p1t6;
     public Label p1t7;
     public Label p1t8;
+    public Label p1t9;
+    public Label p1t10;
+    public Label p1t11;
+    public Label p1t12;
+    public Label p1t13;
+    public Label p1t14;
+    public Label p1t15;
+    public Label p1t16;
+    public Label p1t17;
+    public Label p2name;
+    public Label p2t1;
+    public Label p2t2;
+    public Label p2t3;
+    public Label p2t4;
+    public Label p2t5;
+    public Label p2t6;
+    public Label p2t7;
+    public Label p2t8;
+    public Label p2t9;
+    public Label p2t10;
+    public Label p2t11;
+    public Label p2t12;
+    public Label p2t13;
+    public Label p2t14;
+    public Label p2t15;
+    public Label p2t16;
+    public Label p2t17;
     public void sayHelloWorld(ActionEvent actionEvent) {
         helloWorld.setText("This is Yahztee!");
-        p1t1.setText("This is Yahztee!");
-        p1t2.setText("This is Yahztee!");
-        p1t3.setText("This is Yahztee!");
-        p1t4.setText("This is Yahztee!");
-        p1t5.setText("This is Yahztee!");
-        p1t6.setText("This is Yahztee!");
-        p1t7.setText("This is Yahztee!");
-        p1t8.setText("This is Yahztee!");
+        p1name.setText("Player 1");
+        p1t1.setText("test");
+        p1t2.setText("test");
+        p1t3.setText("test");
+        p1t4.setText("test");
+        p1t5.setText("test");
+        p1t6.setText("test");
+        p1t7.setText("test");
+        p1t8.setText("test");
+        p1t9.setText("test");
+        p1t10.setText("test");
+        p1t11.setText("test");
+        p1t12.setText("test");
+        p1t13.setText("test");
+        p1t14.setText("test");
+        p1t15.setText("test");
+        p1t16.setText("test");
+        p1t17.setText("test");
+
+        p2name.setText("Player 2");
+        p2t1.setText("test");
+        p2t2.setText("test");
+        p2t3.setText("test");
+        p2t4.setText("test");
+        p2t5.setText("test");
+        p2t6.setText("test");
+        p2t7.setText("test");
+        p2t8.setText("test");
+        p2t9.setText("test");
+        p2t10.setText("test");
+        p2t11.setText("test");
+        p2t12.setText("test");
+        p2t13.setText("test");
+        p2t14.setText("test");
+        p2t15.setText("test");
+        p2t16.setText("test");
+        p2t17.setText("test");
     }
 
     public void exitToMenu(ActionEvent actionEvent) throws IOException {
@@ -41,5 +98,13 @@ public class GameController {
         Scene scene = new Scene(parent, 500, 500);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public void holdDie(ActionEvent actionEvent) {
+
+    }
+
+    public void rollDice(ActionEvent actionEvent) {
+
     }
 }
