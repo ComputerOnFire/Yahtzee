327e62e052ad03b18ee736b69d9e541b2c01486f
John <computeronfire@protonmail.com>
2021-03-22 03:05:49 -0500

    add variables face and held, add function rollDie

--- src/main/java/com/github/computeronfire/yahtzee/Die.java
+++ src/main/java/com/github/computeronfire/yahtzee/Die.java
@@ -1,4 +1,12 @@
 package com.github.computeronfire.yahtzee;
 
+import java.util.Random;
+
 public class Die {
+    private int face;
+    private boolean held = false;
+
+    public void rollDie(){
+        this.face = new Random().nextInt(6);
+    }
 }
