28adb81e43ffbdbd98fd190d1bf7f9bcb08c30bd
John <computeronfire@protonmail.com>
2021-03-26 00:43:27 -0500

    refactor dice array

--- src/main/java/com/github/computeronfire/yahtzee/Dice.java
+++ src/main/java/com/github/computeronfire/yahtzee/Dice.java
@@ -5,10 +5,10 @@ public class Dice {
      * Constructs an array of 5 dice.
      * Represents each die on the board.
      */
-    public Die[] dice = new Die[5];
+    public Die[] diceArray = new Die[5];
     public Dice() {
-        for (int i = 0; i < dice.length; i++) {
-            dice[i] = new Die();
+        for (int i = 0; i < diceArray.length; i++) {
+            diceArray[i] = new Die();
         }
     }
 }
