7edad0f4d359010d9aa9b32f1a0b2aa07ff87927
John <computeronfire@protonmail.com>
2021-03-25 23:46:34 -0500

    make face and held variables public

--- src/main/java/com/github/computeronfire/yahtzee/Die.java
+++ src/main/java/com/github/computeronfire/yahtzee/Die.java
@@ -3,8 +3,8 @@ package com.github.computeronfire.yahtzee;
 import java.util.Random;
 
 public class Die {
-    private int face;
-    private boolean held = false;
+    public int face;
+    public boolean held = false;
 
     public void rollDie(){
         this.face = new Random().nextInt(6);
