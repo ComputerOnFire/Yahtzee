22197703bf9068be5c15bdc46aa98cdb4783dcac
John <computeronfire@protonmail.com>
2021-04-03 05:06:16 -0500

    commentation

--- src/main/java/com/github/computeronfire/yahtzee/Game.java
+++ src/main/java/com/github/computeronfire/yahtzee/Game.java
@@ -1,14 +1,16 @@
 package com.github.computeronfire.yahtzee;
 
 /**
- * Handles calculations for the game, such as scores.
+ * Handles logic and calculations for the game, such as scores.
  * Contains an array of int to represent the dice face values.
  * Requirements: Game Functionality, Tally Scores, Follow Yahtzee Rules
  */
 
 public class Game {
     private int[] dice;
-
+    /**
+     * Constructs an array of dice values (int 1-6) that will be used to calculate the scores.
+     * */
     public Game (int[] dice){
         this.dice = dice;
     }
