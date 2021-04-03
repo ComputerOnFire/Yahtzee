5b1aa01642f1e5a95715cefdbcc07be8d957163a
John <computeronfire@protonmail.com>
2021-04-03 05:08:42 -0500

    import map, hashmap, arrays

--- src/main/java/com/github/computeronfire/yahtzee/Game.java
+++ src/main/java/com/github/computeronfire/yahtzee/Game.java
@@ -1,5 +1,9 @@
 package com.github.computeronfire.yahtzee;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Handles logic and calculations for the game, such as scores.
  * Contains an array of int to represent the dice face values.
