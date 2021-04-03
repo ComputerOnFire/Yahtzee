edc661186201690b126b97b10674abc11f4a4b17
John <computeronfire@protonmail.com>
2021-04-03 05:08:53 -0500

    add sum and frequency functions

--- src/main/java/com/github/computeronfire/yahtzee/Game.java
+++ src/main/java/com/github/computeronfire/yahtzee/Game.java
@@ -18,4 +18,63 @@ public class Game {
     public Game (int[] dice){
         this.dice = dice;
     }
+
+    private int sum(){ //return sum of all dice
+        int sum = 0;
+        for (int face : dice){
+            sum += face;
+        }
+        return sum;
+    }
+
+    private int faceSum(int value){//return sum of a all dice with certain face value
+        int sum = 0;
+        for (int face : dice){
+            if (face == value){
+                sum += face;
+            }
+        }
+        return sum;
+    }
+
+    private int xOfAKind(int x){ //returns the sum of a X of a kind combination of dice, (3 of a kind, 4 of a kind) TODO: implement xOfAKind
+        return x;
+    }
+
+    private Map<Integer, Integer> repitition() {//calculates how many times each die repeats
+        Map<Integer, Integer> repititions = new HashMap<Integer, Integer>;
+        for (int die : dice){
+            Integer repitition = repititions.get(die);
+            if(repitition == null){
+                repititions.put(die, 1);
+            }
+            else{
+                repititions.put(die, repitition + 1);
+            }
+        }
+        return repititions;
+    }
+
+    private int mostFrequentDie(int[] dice){//returns value of the most frequently repeating die TODO: will be re-written with repititon map
+        Arrays.sort(dice);
+        int current = 1;
+        int maximum = 1;
+        int mostFrequent = dice[0];
+        int last = dice[0];
+
+        for (int i = 0; i < dice.length - 1; i++){
+            if (dice[0] == last){
+                current++;
+            }
+            else{
+                if (current > maximum){
+                    mostFrequent = dice[i];
+                    maximum = current;
+                }
+                last = dice[i+1];
+                current = 1;
+            }
+        }
+        return mostFrequent;
+    }
 }
