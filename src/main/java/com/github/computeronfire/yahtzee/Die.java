df850fa87f64c14033f4a9b9683b89490ee9d387
John <computeronfire@protonmail.com>
2021-04-04 04:11:15 -0500

    make die variables private, add getter functions (best practice)

--- src/main/java/com/github/computeronfire/yahtzee/Die.java
+++ src/main/java/com/github/computeronfire/yahtzee/Die.java
@@ -3,13 +3,19 @@ package com.github.computeronfire.yahtzee;
 import java.util.Random;
 
 public class Die {
-    public int face;
-    public boolean held = false;
+    private int face;
+    private boolean held = false;
 
     public void rollDie(){
         this.face = new Random().nextInt(6) + 1;
     }
     public void hold(boolean held){
         this.held = held;
+    }
+    public int getFace(){
+        return face;
+    }
+    public boolean isHeld(){
+        return held;
     }
 }
