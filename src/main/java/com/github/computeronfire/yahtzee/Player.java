5d3991082f29d6f38cae04d151a6217c1bb36590
John <computeronfire@protonmail.com>
2021-04-06 23:52:52 -0500

    implement Player class

--- src/main/java/com/github/computeronfire/yahtzee/Player.java
+++ src/main/java/com/github/computeronfire/yahtzee/Player.java
@@ -1,4 +1,18 @@
 package com.github.computeronfire.yahtzee;
 
 public class Player {
+    private final String name;
+    private Score[] scoreCard = new Score[20];
+    public Player(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public Score[] getScores(){
+        return scoreCard;
+    }
+    public void selectScore(Score score, int index){
+        scoreCard[index] = score;
+    }
 }
