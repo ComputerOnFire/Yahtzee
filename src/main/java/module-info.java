0e85315b547aa6cfc013b171ef7d682806c47cc5
John <computeronfire@protonmail.com>
2021-04-09 05:58:16 -0500

    gradle config and module-info for modular approach

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module Yahtzee {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.github.computeronfire.yahtzee to javafx.fxml;
+    exports com.github.computeronfire.yahtzee;
+}
