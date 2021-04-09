71f72cce2cbf7bb531707335d43cd075f819ff0f
John <computeronfire@protonmail.com>
2021-04-09 05:53:17 -0500

    convert from module to standalone application

---
+++ src/main/java/com/github/computeronfire/yahtzee/Launcher.java
@@ -0,0 +1,7 @@
+package com.github.computeronfire.yahtzee;
+
+public class Launcher {
+    public static void main(String[] args){
+        com.github.computeronfire.yahtzee.Main.main(args);
+    }
+}
