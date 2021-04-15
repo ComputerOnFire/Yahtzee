14b3d8824a52fbc2b721b745eaed0cb14a3ed20f
John <computeronfire@protonmail.com>
2021-04-15 00:53:20 -0500

    swap launcher and main class functionalities to better suit names

--- src/main/java/com/github/computeronfire/yahtzee/Launcher.java
+++ src/main/java/com/github/computeronfire/yahtzee/Launcher.java
@@ -1,7 +1,26 @@
 package com.github.computeronfire.yahtzee;
 
-public class Launcher {
-    public static void main(String[] args){
-        com.github.computeronfire.yahtzee.Main.main(args);
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+ /**
+   * TODO:
+   * requirements should match requirements and include document index number
+   *
+   */
+
+public class Launcher extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/yahtzeeMenu.fxml"));
+        primaryStage.setTitle("Yahtzee!");
+        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
     }
 }
--- src/main/java/com/github/computeronfire/yahtzee/Main.java
+++ src/main/java/com/github/computeronfire/yahtzee/Main.java
@@ -1,25 +1,7 @@
 package com.github.computeronfire.yahtzee;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
- /**
-   * TODO:
-   * requirements should match requirements and include document index number
-   */
-
-public class Main extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/yahtzeeMenu.fxml"));
-        primaryStage.setTitle("Yahtzee!");
-        primaryStage.setScene(new Scene(root, 1000, 750));
-        primaryStage.show();
-    }
-    public static void main(String[] args) {
-        launch(args);
+public class Main {
+    public static void main(String[] args){
+        Launcher.main(args);
     }
 }
