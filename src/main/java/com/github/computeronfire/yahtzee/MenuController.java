92e2196d73455c72f279c1e1c6268521f7124c69
John <computeronfire@protonmail.com>
2021-04-15 00:37:51 -0500

    implement player name fields into startGame function

--- src/main/java/com/github/computeronfire/yahtzee/MenuController.java
+++ src/main/java/com/github/computeronfire/yahtzee/MenuController.java
@@ -5,10 +5,12 @@
 package com.github.computeronfire.yahtzee;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,11 +19,32 @@ import java.util.List;
 
 public class MenuController {
 
+    @FXML
+    private TextField player1;
+    @FXML
+    private TextField player2;
+    @FXML
+    private TextField player3;
+    @FXML
+    private TextField player4;
+    @FXML
+    private TextField player5;
+    @FXML
+    private TextField player6;
+    @FXML
+    private TextField player7;
+
+    private TextField[] playerFields;
+
     public void startGame(ActionEvent actionEvent) throws IOException {
+        playerFields = new TextField[]{player1,player2,player3,player4,player5,player6,player7};
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/yahtzeeGame.fxml"));
         List<Player> players = new ArrayList<>();
-        players.add(new Player("Player 1"));
-        players.add(new Player("Player 2"));
+        for (TextField player : playerFields){
+            if(!player.getText().isEmpty()){
+                players.add(new Player(player.getText()));
+            }
+        }
         Parent parent = fxmlLoader.load();
         GameController controller = fxmlLoader.getController();
         controller.initializeBoard(players);
