cb2507a1bf082c9bbeb0c37744f0f40d7dfea672
John <computeronfire@protonmail.com>
2021-04-22 21:25:54 -0500

    refactor largeStraightTest(), implement notLargeStraightTest()

--- src/test/java/com/github/computeronfire/yahtzee/ScoreCardTest.java
+++ src/test/java/com/github/computeronfire/yahtzee/ScoreCardTest.java
@@ -40,30 +40,21 @@ public class ScoreCardTest {
     }
 
     @Test
-    public void largeStraightTest(){
-        Dice dice = new Dice();
+    public void largeStraightTest(){//tests if largeStraight returns the correct values given the dice array
+        Die[] dice = new Die[]{new Die(1),new Die(2), new Die(3), new Die(4),new Die(5)};
         Player testPlayer = new Player("Test");
-        dice.rollDice();
-        while(!(dice.getDie(0).getFace() == 1 && dice.getDie(1).getFace() == 2 && dice.getDie(2).getFace() == 3 && dice.getDie(3).getFace() == 4 && dice.getDie(4).getFace() == 5)){
-            dice.rollDice();
-        }
-        ScoreCard sCard = new ScoreCard(testPlayer, dice.getDice());
+        ScoreCard sCard = new ScoreCard(testPlayer, dice);
         sCard.calculateScores();
         assertEquals(15, sCard.getScore(12).getValue());
         assertEquals(20, sCard.getScore(13).getValue());
+    }
+    @Test
+    public void notLargeStraightTest(){
+        Die[] dice = new Die[]{new Die(1),new Die(2), new Die(3), new Die(4),new Die(4)};
+        Player testPlayer = new Player("Test");
+        ScoreCard sCard = new ScoreCard(testPlayer, dice);
+        sCard.calculateScores();
+        assertEquals(0, sCard.getScore(13).getValue());
     }
 
-    @Test
-    public void calculateStraightTest(){
-        Dice dice = new Dice();
-        Player testPlayer = new Player("Test");
-        dice.rollDice();
-        while(!(dice.getDie(0).getFace() == 2 && dice.getDie(1).getFace() == 3 && dice.getDie(2).getFace() == 4 && dice.getDie(3).getFace() == 5 && dice.getDie(4).getFace() == 6)){
-            dice.rollDice();
-        }
-        ScoreCard sCard = new ScoreCard(testPlayer, dice.getDice());
-        sCard.calculateScores();
-        assertEquals(15, sCard.getScore(12).getValue());
-        assertEquals(20, sCard.getScore(13).getValue());
-    }
 }
