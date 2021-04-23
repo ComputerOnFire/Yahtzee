ed6c81b652b522fbd5e087537435d03fc409ee98
John <computeronfire@protonmail.com>
2021-04-22 21:25:15 -0500

    implement fullHouseTest, notFullHouseTest

--- src/test/java/com/github/computeronfire/yahtzee/ScoreCardTest.java
+++ src/test/java/com/github/computeronfire/yahtzee/ScoreCardTest.java
@@ -5,6 +5,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ScoreCardTest {
+    @Test public void fullHouseTest(){//tests if fullHouse returns the correct values given the dice array
+        Die[] dice = new Die[]{new Die(2),new Die(2), new Die(3), new Die(3),new Die(3)};
+        Player testPlayer = new Player("Test");
+        ScoreCard sCard = new ScoreCard(testPlayer, dice);
+        sCard.calculateScores();
+        assertEquals(25, sCard.getScore(11).getValue());
+    }
+    @Test public void notFullHouseTest(){
+        Die[] dice = new Die[]{new Die(2),new Die(2), new Die(3), new Die(3),new Die(6)};
+        Player testPlayer = new Player("Test");
+        ScoreCard sCard = new ScoreCard(testPlayer, dice);
+        sCard.calculateScores();
+        assertEquals(0, sCard.getScore(11).getValue());
+    }
 
     @Test
     public void smallStraightTest(){
