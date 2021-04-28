40e4fd243ca3acf603fb659eb8dd8d210629e7ce
John <computeronfire@protonmail.com>
2021-04-28 07:42:44 -0500

    refactor test cases to use scoreCard setup function

--- src/test/java/com/github/computeronfire/yahtzee/ScoreCardTest.java
+++ src/test/java/com/github/computeronfire/yahtzee/ScoreCardTest.java
@@ -6,163 +6,125 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ScoreCardTest {
 
-    @Test
-    public void onesTest(){
-        Die[] dice = new Die[]{new Die(1),new Die(2), new Die(1), new Die(4),new Die(6)};
+    private ScoreCard scoreSetup(int die1, int die2, int die3, int die4, int die5){//sets up a score card for the given dice faces, useful for testing
+        Die[] dice = new Die[]{new Die(die1),new Die(die2), new Die(die3), new Die(die4),new Die(die5)};
         Player testPlayer = new Player("Test");
         ScoreCard sCard = new ScoreCard(testPlayer, dice);
         sCard.calculateScores();
-        assertEquals(2, sCard.getScore(0).getValue());
+        return sCard;
+    }
 
+    @Test
+    public void onesTest(){
+        ScoreCard sCard = scoreSetup(1,2,1,4,6);
+        assertEquals(2, sCard.getScore(0).getValue());
     }
     @Test
     public void noOnesTest(){
-        Die[] dice = new Die[]{new Die(2),new Die(2), new Die(2), new Die(4),new Die(6)};
-        Player testPlayer = new Player("Test");
-        ScoreCard sCard = new ScoreCard(testPlayer, dice);
-        sCard.calculateScores();
+        ScoreCard sCard = scoreSetup(2,2,2,4,6);
         assertEquals(0, sCard.getScore(0).getValue());
     }
 
     @Test
     public void twosTest(){
-        Die[] dice = new Die[]{new Die(1),new Die(2), new Die(2), new Die(4),new Die(6)};
-        Player testPlayer = new Player("Test");
-        ScoreCard sCard = new ScoreCard(testPlayer, dice);
-        sCard.calculateScores();
+        ScoreCard sCard = scoreSetup(1,2,2,4,6);
         assertEquals(4, sCard.getScore(1).getValue());
     }
 
     @Test
     public void noTwosTest(){
-        Die[] dice = new Die[]{new Die(1),new Die(1), new Die(1), new Die(4),new Die(6)};
-        Player testPlayer = new Player("Test");
-        ScoreCard sCard = new ScoreCard(testPlayer, dice);
-        sCard.calculateScores();
+        ScoreCard sCard = scoreSetup(1,1,1,4,6);
         assertEquals(0, sCard.getScore(1).getValue());
     }
 
     @Test
     public void threesTest(){
-        Die[] dice = new Die[]{new Die(3),new Die(2), new Die(3), new Die(4),new Die(6)};
-        Player testPlayer = new Player("Test");
-        ScoreCard sCard = new ScoreCard(testPlayer, dice);
-        sCard.calculateScores();
+        ScoreCard sCard = scoreSetup(3,2,3,4,6);
         assertEquals(6, sCard.getScore(2).getValue());
     }
     @Test
     public void noThreesTest(){
-        Die[] dice = new Die[]{new Die(4),new Die(2), new Die(5), new Die(4),new Die(6)};
-        Player testPlayer = new Player("Test");
-        ScoreCard sCard = new ScoreCard(testPlayer, dice);
-        sCard.calculateScores();
+        ScoreCard sCard = scoreSetup(4,2,5,4,6);
         assertEquals(0, sCard.getScore(2).getValue());
     }
 
     @Test
     public void foursTest(){
-        Die[] dice = new Die[]{new Die(1),new Die(2), new Die(4), new Die(4),new Die(6)};
-        Player testPlayer = new Player("Test");
-        ScoreCard sCard = new ScoreCard(testPlayer, dice);
-        sCard.calculateScores();
+        ScoreCard sCard = scoreSetup(1,2,4,4,6);
         assertEquals(8, sCard.getScore(3).getValue());
     }
     @Test
     public void noFoursTest(){
-        Die[] dice = new Die[]{new Die(1),new Die(2), new Die(3), new Die(5),new Die(6)};
-        Player testPlayer = new Player("Test");
-        ScoreCard sCard = new ScoreCard(testPlayer, dice);
-        sCard.calculateScores();
+        ScoreCard sCard = scoreSetup(1,2,3,5,6);
         assertEquals(0, sCard.getScore(3).getValue());
     }
 
     @Test
     public void fivesTest(){
-        Die[] dice = new Die[]{new Die(1),new Die(2), new Die(1), new Die(5),new Die(5)};
-        Player testPlayer = new Player("Test");
-        ScoreCard sCard = new ScoreCard(testPlayer, dice);
-        sCard.calculateScores();
+        ScoreCard sCard = scoreSetup(1,2,1,5,5);
         assertEquals(10, sCard.getScore(4).getValue());
     }
 
     @Test
     public void noFivesTest(){
-        Die[] dice = new Die[]{new Die(1),new Die(2), new Die(1), new Die(4),new Die(6)};
-        Player testPlayer = new Player("Test");
-        ScoreCard sCard = new ScoreCard(testPlayer, dice);
-        sCard.calculateScores();
+        ScoreCard sCard = scoreSetup(1,2,1,4,6);
         assertEquals(0, sCard.getScore(4).getValue());
     }
 
     @Test
     public void sixesTest(){
-        Die[] dice = new Die[]{new Die(6),new Die(2), new Die(1), new Die(4),new Die(6)};
-        Player testPlayer = new Player("Test");
-        ScoreCard sCard = new ScoreCard(testPlayer, dice);
-        sCard.calculateScores();
+        ScoreCard sCard = scoreSetup(6,2,1,4,6);
         assertEquals(12, sCard.getScore(5).getValue());
     }
 
     @Test
     public void noSixesTest(){
-        Die[] dice = new Die[]{new Die(3),new Die(2), new Die(1), new Die(4),new Die(5)};
-        Player testPlayer = new Player("Test");
-        ScoreCard sCard = new ScoreCard(testPlayer, dice);
-        sCard.calculateScores();
+        ScoreCard sCard = scoreSetup(1,2,1,4,1);
         assertEquals(0, sCard.getScore(5).getValue());
     }
-
+    @Test
+    public void threeOfAKindTest(){
+        ScoreCard sCard = scoreSetup(1,1,1,4,4);
+        assertEquals(0, sCard.getScore(9).getValue());
+    }
+    @Test
+    public void fourOfAKindTest(){
+        ScoreCard sCard = scoreSetup(1,1,1,1,4);
+        assertEquals(0, sCard.getScore(10).getValue());
+    }
     @Test
     public void fullHouseTest(){//tests if fullHouse returns the correct values given the dice array
-        Die[] dice = new Die[]{new Die(2),new Die(2), new Die(3), new Die(3),new Die(3)};
-        Player testPlayer = new Player("Test");
-        ScoreCard sCard = new ScoreCard(testPlayer, dice);
-        sCard.calculateScores();
+        ScoreCard sCard = scoreSetup(2,2,3,3,3);
         assertEquals(25, sCard.getScore(11).getValue());
     }
     @Test
     public void notFullHouseTest(){
-        Die[] dice = new Die[]{new Die(2),new Die(2), new Die(3), new Die(3),new Die(6)};
-        Player testPlayer = new Player("Test");
-        ScoreCard sCard = new ScoreCard(testPlayer, dice);
-        sCard.calculateScores();
+        ScoreCard sCard = scoreSetup(2,2,3,3,6);
         assertEquals(0, sCard.getScore(11).getValue());
     }
 
     @Test
     public void smallStraightTest(){//tests if smallStraight returns the correct values given the dice array
-        Die[] dice = new Die[]{new Die(1),new Die(2), new Die(3), new Die(4),new Die(4)};
-        Player testPlayer = new Player("Test");
-        ScoreCard sCard = new ScoreCard(testPlayer, dice);
-        sCard.calculateScores();
+        ScoreCard sCard = scoreSetup(1,2,3,4,4);
         assertEquals(30, sCard.getScore(12).getValue());
         assertEquals(0, sCard.getScore(13).getValue());
     }
     @Test
     public void notSmallStraightTest(){
-        Die[] dice = new Die[]{new Die(1),new Die(2), new Die(3), new Die(3),new Die(3)};
-        Player testPlayer = new Player("Test");
-        ScoreCard sCard = new ScoreCard(testPlayer, dice);
-        sCard.calculateScores();
+        ScoreCard sCard = scoreSetup(1,2,3,3,3);
         assertEquals(0, sCard.getScore(12).getValue());
         assertEquals(0, sCard.getScore(13).getValue());
     }
 
     @Test
     public void largeStraightTest(){//tests if largeStraight returns the correct values given the dice array
-        Die[] dice = new Die[]{new Die(1),new Die(2), new Die(3), new Die(4),new Die(5)};
-        Player testPlayer = new Player("Test");
-        ScoreCard sCard = new ScoreCard(testPlayer, dice);
-        sCard.calculateScores();
+        ScoreCard sCard = scoreSetup(1,2,3,4,5);
         assertEquals(30, sCard.getScore(12).getValue());
         assertEquals(40, sCard.getScore(13).getValue());
     }
     @Test
     public void notLargeStraightTest(){
-        Die[] dice = new Die[]{new Die(1),new Die(2), new Die(3), new Die(4),new Die(4)};
-        Player testPlayer = new Player("Test");
-        ScoreCard sCard = new ScoreCard(testPlayer, dice);
-        sCard.calculateScores();
+        ScoreCard sCard = scoreSetup(1,2,3,4,4);
         assertEquals(0, sCard.getScore(13).getValue());
     }
 }
