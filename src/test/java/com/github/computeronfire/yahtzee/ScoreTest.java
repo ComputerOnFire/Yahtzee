c8461e1eec41ff0411619c3fbe75512976e59fa0
John <computeronfire@protonmail.com>
2021-04-28 08:17:57 -0500

    implement tests for Score.java class

--- src/test/java/com/github/computeronfire/yahtzee/ScoreTest.java
+++ src/test/java/com/github/computeronfire/yahtzee/ScoreTest.java
@@ -1,4 +1,26 @@
 package com.github.computeronfire.yahtzee;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ScoreTest {
+    @Test
+    public void scoreTest(){//tests if the score set is correctly returned
+        Score score = new Score();
+        score.setScore(1);
+        assertEquals(1, score.getValue());
+    }
+    @Test
+    public void retainTest(){//tests if the score set is correctly returned
+        Score score = new Score();
+        score.retainScore();
+        assertEquals(true, score.isRetained());
+    }
+    @Test
+    public void totalOrBonusTest(){//tests if the score set is correctly returned
+        Score score = new Score();
+        score.markTotalOrBonus();
+        assertEquals(false, score.isNotTotalOrBonus());
+    }
 }
